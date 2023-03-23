3d7346c93ee007776d786b688293637ed3ec6ebf
FranQ <frankchristopherquispe@gmail.com>
2023-03-23 00:32:25 -0500

    playing with urls prefix at index.html and both urls.py

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 
-
+app_name="my-polls"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:question_id>/details", views.details, name="details"),
-    path("<int:question_id>/results", views.results, name="results"),
-    path("<int:question_id>/votes", views.vote, name="votes"),
+    path("<int:question_id>/detalles", views.details, name="details"),
+    path("<int:question_id>/resultados", views.results, name="results"),
+    path("<int:question_id>/votos", views.vote, name="votes"),
 ]
