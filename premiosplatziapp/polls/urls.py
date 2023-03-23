019f7c423057df82f67ea9d1b138734d54f58cd9
FranQ <frankchristopherquispe@gmail.com>
2023-03-23 12:25:37 -0500

    this basic form looks cool ^^

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 
-app_name="my-polls"
+app_name="polls"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:question_id>/detalles", views.details, name="details"),
