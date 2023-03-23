62e7a4689cf31ff8bd0bb47f8c03d6ed4ada8f8f
FranQ <frankchristopherquispe@gmail.com>
2023-03-22 20:55:02 -0500

    playing with basic views and templates ^^

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+
 
 def index(request):
-    return HttpResponse('pagina principal')
+    latest_question_list = Question.objects.all()
+    return render(request, "polls/index.html", { "latest_question_list": latest_question_list })
 
 def details(request, question_id):
     return HttpResponse(f'estas viendo los detalles de {question_id}')
