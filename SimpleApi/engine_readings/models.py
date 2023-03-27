adee8a73119bf3dbff8cb9e842cfe66551cf1871
FranQ <frankchristopherquispe@gmail.com>
2023-03-27 11:24:02 -0500

    Add Engine and Readings in models

---
+++ SimpleApi/engine_readings/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class Engine(models.Model):
+    objects = models.Manager
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'Motor: ({self.name})'
+
+class Readings(models.Model):
+    objects = models.Manager()
+    value = models.IntegerField()
+    engine = models.ForeignKey(Engine, on_delete= models.CASCADE)
+
+    def __str__(self):
+        return f'{self.engine}: {self.pk}-{self.value}'
+
